aa07c4f6030dcb3e27f3e4ca030d75768d3cb16f
druid <angular.druid@gmail.com>
2022-08-29 21:23:22 -0400

    [tz-1] Update

--- projects/spear/src/environments/environment.ts
+++ projects/spear/src/environments/environment.ts
@@ -102,7 +102,7 @@ export const environment = {
   }
 };
 
-const x = 'xxxxxxxxxxxxxxxxxxxxxxxsxxxx';
+const x = 'xxxxxxxxxxxxxxxxxxxxxxxxsxxxx';
 
 /*
  * For easier debugging in development mode, you can import the following file
